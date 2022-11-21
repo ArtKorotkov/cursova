2625d05211b42c46d29405ee034f34316608f06e
Artem <tiomich227@gmail.com>
2022-11-21 09:40:53 +0200

    clear and fix, also added position stycki for navbar

---
+++ app.js
@@ -0,0 +1,8 @@
+$('.gabarytu, .closemodal').click(function (e) {
+    e.preventDefault();
+    $('.kadobagud').toggleClass('midsalod');
+});
+// $('.closemodal').click(function (e) {
+//     e.preventDefault();
+//     $('.kadobagud').removeClass('midsalod');
+// });
