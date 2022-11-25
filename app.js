a3043f9ff223add1da5545c32f59723a72ac5200
Artem <tiomich227@gmail.com>
2022-11-25 10:28:02 +0200

    додана зміна кольору. додана нова вьорстка. додані нові картінки.

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ $('.gabarytu, .closemodal').click(function (e) {
 //     e.preventDefault();
 //     $('.kadobagud').removeClass('midsalod');
 // });
+
+
+const colors = document.querySelectorAll('.color_item')
+const mainImg = document.querySelector('.main_img img')
+
+colors.forEach((color) => {
+    color.addEventListener('click', function () {
+        colors.forEach(color => color.classList.remove('active'))
+        color.classList.add('active')
+        mainImg.setAttribute('src', color.getAttribute('main-src'))
+    })
+})
